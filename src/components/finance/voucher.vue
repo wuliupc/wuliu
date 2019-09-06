ad82e5f35aa7c910dc1a312cd247f935a20a67c4
孙学志 <983517872@qq.com>
2019-09-06 16:57:50 +0800

    444

--- src/components/finance/voucher.vue
+++ src/components/finance/voucher.vue
@@ -1,7 +1,13 @@
 <template>
+	<div id="app">33333</div>
 </template>
-
 <script>
+	export default {
+		name: 'app',
+		components: {
+	
+		}
+	}
 </script>
 
 <style>
--- src/router/router.js
+++ src/router/router.js
@@ -8,29 +8,23 @@ import login from '../components/login.vue'; //登录
 import register from '../components/register.vue'; //注册
 import statistics_home from '../components/statistics/home.vue'; //统计端首页
 import statistics_buy from '../components/statistics/buy.vue';
-import statistics_recode from '../components/statistics/recode.vue';
-import statistics_user from '../components/statistics/user.vue';
 import finance_index from '../components/finance/index.vue'; //财务首页
 import finance_payment from '../components/finance/payment.vue'; //财务待结款
 import finance_voucher from '../components/finance/voucher.vue'; //财务待传凭证
 import finance_pay from '../components/finance/pay.vue'; //财务已结款
 import finance_invoice from '../components/finance/invoice.vue'; //财务已开票
+// import finance_immediate_payment from '../components/finance/immediate_payment.vue'; //财务立即支付
+// import finance_pay_success from '../components/finance/pay_success.vue'; //财务支付成功
 // 配置路由
 const routes = [{
 		path: '/statistics_home',
 		component: statistics_home,
 		children: [{
-			path: '*',
-			redirect: '/statistics_buy'
-		}, {
 			path: '/statistics_buy',
 			component: statistics_buy
-		},{
-			path: '/statistics_recode',
-			component: statistics_recode
-		},{
-			path: '/statistics_user',
-			component: statistics_user
+		}, {
+			path: '*',
+			redirect: '/statistics_buy'
 		}]
 	}, {
 		path: '/register',
@@ -45,20 +39,28 @@ const routes = [{
 	{
 		path: '/finance_index',
 		component: finance_index,
-		children: [{
-				path: '*',
-				redirect: '/finance_payment'
-			}, {
-				path: '/finance_voucher',
-				component: finance_voucher
-			}, {
-				path: '/finance_pay',
-				component: finance_pay
-			},
-			{
-				path: '/finance_invoice',
-				component: finance_invoice
-			}
+		redirect: '/finance_payment',
+		children: [ 
+		{
+			path: '/finance_payment',
+			component: finance_payment,
+		}, {
+			path: '/finance_voucher',
+			component: finance_voucher
+		}, {
+			path: '/finance_pay',
+			component: finance_pay
+		}, {
+			path: '/finance_invoice',
+			component: finance_invoice
+		},
+		// {
+		// 	path: '/finance_immediate_payment',
+		// 	component: finance_immediate_payment,
+		// },{
+		// 	path: '/finance_pay_success',
+		// 	component: finance_pay_success,
+		// }
 		]
 	}
 ]
