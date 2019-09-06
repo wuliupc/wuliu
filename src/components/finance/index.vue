18a3708d4e3db0c5435441205b1816e5b3bc4c90
孙学志 <983517872@qq.com>
2019-09-06 14:47:09 +0800

    111

---
+++ src/components/finance/index.vue
@@ -0,0 +1,71 @@
+<template>
+	<div id="app">
+		<el-container>
+			<el-header>
+				<div class="header flex">
+					<ul class="nav flex tc f18 c333">
+						<li class="active"><router-link to='/finance_payment' class="white">待结款</router-link></li>
+						<li><router-link to='/finance_voucher'>待传凭证</router-link></li>
+						<li><router-link to='/finance_pay'>已结款</router-link></li>
+						<li><router-link to='/finance_invoce'>已开票</router-link></li>
+					</ul>
+					<div class="f18 c666 user flex">
+						<img src="../../assets/img/user.png" />
+						<p>13******99</p>
+					</div>
+				</div>
+				
+			</el-header>
+			<el-main>
+			  <router-view></router-view>	
+			</el-main>
+		</el-container>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'app',
+		components: {
+	
+		}
+	}
+</script>
+
+<style>
+	.header{
+		width: 1200px;
+		margin: 0 auto;
+		justify-content: space-between;
+	}
+	.el-header {
+		background-color: #fff;
+		color: #333;
+		text-align: center;
+		line-height: 60px;
+	}
+	.el-main {
+		background-color: #F8F8F8;
+		color: #333;
+		text-align: center;
+		line-height: 160px;
+	}
+	body>.el-container {
+		margin-bottom: 40px;
+	}
+	.nav>li {
+		width: 104px;
+	}
+	.active {
+		background-color: #08b963;
+		color: #FFFFFF;
+	}
+	.user{
+		align-items: center;
+	}
+	.user img{
+		width: 24px;
+		height: 29px;
+		margin-right: 10px;
+	}
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -8,27 +8,52 @@ import login from '../components/login.vue'; //登录
 import register from '../components/register.vue'; //注册
 import statistics_home from '../components/statistics/home.vue'; //统计端首页
 import statistics_buy from '../components/statistics/buy.vue';
+import finance_index from '../components/finance/index.vue'; //财务首页
+import finance_payment from '../components/finance/payment.vue'; //财务待结款
+import finance_voucher from '../components/finance/voucher.vue'; //财务待传凭证
+import finance_pay from '../components/finance/pay.vue'; //财务已结款
+import finance_invoice from '../components/finance/invoice.vue'; //财务已开票
 // 配置路由
-const routes = [ {
-	path: '/statistics_home',
-	component: statistics_home,
-	children:[{
-	path: '/statistics_buy',
-	component: statistics_buy
-},{
-	path: '*',
-	redirect: '/statistics_buy'
-}]
-}, {
-	path: '/register',
-	component: register
-}, {
-	path: '/login',
-	component: login
-}, {
-	path: '*',
-	redirect: '/login'
-} /*默认跳转路由*/ ]
+const routes = [{
+		path: '/statistics_home',
+		component: statistics_home,
+		children: [{
+			path: '/statistics_buy',
+			component: statistics_buy
+		}, {
+			path: '*',
+			redirect: '/statistics_buy'
+		}]
+	}, {
+		path: '/register',
+		component: register
+	}, {
+		path: '/login',
+		component: login
+	}, {
+		path: '*',
+		redirect: '/login'
+	}, /*默认跳转路由*/
+	{
+		path: '/finance_index',
+		component: finance_index,
+		children: [{
+			path: '*',
+			redirect: '/finance_payment'
+		}, {
+			path: '/finance_voucher',
+			component: finance_voucher
+		}, {
+			path: '/finance_pay',
+			component: finance_pay
+		}, 
+		{
+			path: '/finance_invoice',
+			component: finance_invoice
+		}
+		]
+	}
+]
 
 
 const router = new VueRouter({
